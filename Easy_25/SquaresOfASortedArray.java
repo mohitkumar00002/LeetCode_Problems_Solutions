631dd17187acd0dae54ab147115abb833e004e62
Mohit Kumar <rajputmohit0002@gmail.com>
2026-01-15 14:04:32 +0530

    Squares of a sorted array

---
+++ Easy_25/SquaresOfASortedArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class SquaresOfASortedArray {
+  public static void main(String[] args) {
+    int[] nums = {-4,-1,0,3,10};
+    System.out.println(Arrays.toString(sortedSquares(nums)));
+  }
+  static int[] sortedSquares(int[] nums) {
+        int[] arr = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+          arr[i] = nums[i] * nums[i];
+          }
+        Arrays.sort(arr);
+        return arr;
+    }
+}
