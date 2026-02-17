a0d0f41aa745999ff965440ddbbf2e9aea49420a
Mohit Kumar <rajputmohit0002@gmail.com>
2026-02-17 22:50:59 +0530

    Running Sum of 1d Array

--- 1_Array_String_Basic/Basic_Traversal/RunningSumOf1DArray_1480.java
+++ 1_Array_String_Basic/Basic_Traversal/RunningSumOf1DArray_1480.java
@@ -12,7 +12,7 @@ public class RunningSumOf1DArray_1480 {
     int sum = 0;
     for(int i=0;i<nums.length;i++){
       sum += nums[i];
-      ans[i] =  sum;
+      ans[i] = sum;
     }
     return ans;
   }
