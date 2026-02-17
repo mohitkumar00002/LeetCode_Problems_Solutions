6d9147fab448f3c949fe232e7a468aeb557e2659
Mohit Kumar <rajputmohit0002@gmail.com>
2026-02-17 23:07:09 +0530

    Squares of a Sorted Array

--- 1_Array_String_Basic/Basic_Traversal/SquaresOfSortedArray_977.java
+++ 1_Array_String_Basic/Basic_Traversal/SquaresOfSortedArray_977.java
@@ -5,6 +5,7 @@ public class SquaresOfSortedArray_977 {
   public static void main(String[] args) {
     int[] nums = { -4, -1, 0, 3, 10 };
     int[] result = sortedSquares(nums);
+    
     for(int num : result){
       System.out.print(num + " ");
     }
