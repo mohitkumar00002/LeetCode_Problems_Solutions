9896d25b50a416b905c79de24155154a60bfe02f
Mohit Kumar <rajputmohit0002@gmail.com>
2026-02-17 23:10:28 +0530

    Maximum Subarray

--- 1_Array_String_Basic/Basic_Traversal/MaximumSubarray_53.java
+++ 1_Array_String_Basic/Basic_Traversal/MaximumSubarray_53.java
@@ -10,10 +10,8 @@ public class MaximumSubarray_53 {
     int maxSum = nums[0];
 
     for (int i = 1; i < nums.length; i++) {
-
       currentSum = Math.max(nums[i], currentSum + nums[i]);
       maxSum = Math.max(maxSum, currentSum);
-
     }
 
     return maxSum;
